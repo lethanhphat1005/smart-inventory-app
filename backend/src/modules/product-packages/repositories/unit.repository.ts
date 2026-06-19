a003146d2b4c29dacdac294920ec3ccf61f81a81
HowardPeter <phuocnt1611@gmail.com>
2026-06-19 22:51:30 +0700

    chore(product-package): add findManyByIds in unit repository

--- backend/src/modules/product-packages/repositories/unit.repository.ts
+++ backend/src/modules/product-packages/repositories/unit.repository.ts
@@ -3,7 +3,7 @@ import { prisma } from '../../../db/prismaClient.js';
 import type { UnitResponseDto } from '../product-package.dto.js';
 
 export class UnitRepository {
-  async findUnitById(unitId: string): Promise<UnitResponseDto | null> {
+  async findOneById(unitId: string): Promise<UnitResponseDto | null> {
     return await prisma.unit.findUnique({
       where: {
         unitId,
@@ -15,6 +15,26 @@ export class UnitRepository {
       },
     });
   }
+
+  async findManyByIds(unitIds: string[]): Promise<UnitResponseDto[]> {
+    if (unitIds.length === 0) {
+      return [];
+    }
+
+    return await prisma.unit.findMany({
+      where: {
+        unitId: {
+          in: unitIds,
+        },
+      },
+      select: {
+        unitId: true,
+        code: true,
+        name: true,
+      },
+    });
+  }
+
   async findAll(): Promise<UnitResponseDto[]> {
     return await prisma.unit.findMany({
       select: {
