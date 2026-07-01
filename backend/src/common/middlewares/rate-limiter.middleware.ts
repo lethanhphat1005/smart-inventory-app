07de4050338b7b46aa4c0e7d7408c2af8613b520
HowardPeter <phuocnt1611@gmail.com>
2026-07-01 23:40:57 +0700

    chore(common): add middleware rate limiter

---
+++ backend/src/common/middlewares/rate-limiter.middleware.ts
@@ -0,0 +1,42 @@
+import rateLimit from 'express-rate-limit';
+import { StatusCodes } from 'http-status-codes';
+
+import { sendResponse } from '../utils/api-response.util.js';
+import { logger } from '../utils/logger.util.js';
+
+export const rateLimiter = ({
+  windowMs = 15 * 60 * 1000,
+  max = 100,
+}: {
+  windowMs?: number;
+  max?: number;
+}) => {
+  return rateLimit({
+    windowMs,
+    max,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+      // thêm log vì pinoLogger đã ignore >= 400 status code
+      logger.warn(
+        {
+          req: {
+            method: req.method,
+            url: req.originalUrl,
+            ip: req.ip,
+          },
+          res: {
+            statusCode: StatusCodes.TOO_MANY_REQUESTS,
+          },
+        },
+        'Rate limit exceeded',
+      );
+
+      sendResponse.error(
+        res,
+        StatusCodes.TOO_MANY_REQUESTS,
+        'Too many requests. Please try again later!',
+      );
+    },
+  });
+};
