2fd3d26c49eeef0ed86df3c55a3626529e22de4e
HowardPeter <phuocnt1611@gmail.com>
2026-07-29 00:35:22 +0700

    chore(opt-sis): add bash file for deploy new ecr image

---
+++ opt-sis/scripts/lambda-build-image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+AWS_REGION="${AWS_REGION:-ap-southeast-1}"
+AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID:?Missing AWS_ACCOUNT_ID}"
+ECR_REPOSITORY="${ECR_REPOSITORY:-storix-backend}"
+IMAGE_TAG="${1:-latest}"
+
+aws ecr get-login-password --region "$AWS_REGION" \
+| docker login \
+    --username AWS \
+    --password-stdin \
+    "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+
+docker buildx build \
+  --builder multiarch-builder \
+  --platform linux/arm64 \
+  --provenance=false \
+  --sbom=false \
+  -f Dockerfile.lambda \
+  -t "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG" \
+  --push \
+  .
